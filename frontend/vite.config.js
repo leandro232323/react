46730ea65a8ca315a7032baa360d81b1458669b8
leandro232323 <cristian2004leandro@gmail.com>
2026-04-10 15:34:31 -0500

    modificaciones finales

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'bookish-adventure-97gj99x6xq73xp9w-5173.app.github.dev'
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })
